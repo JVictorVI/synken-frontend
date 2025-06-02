1f3138ed060600d23b675fda93d03aaf33ca2742
João Victor <jvictordomingos123@gmail.com>
2025-06-01 21:16:45 -0300

    Correções

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,14 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Post from "../Post/Post";
 import { getPosts } from "./FeedService";
 
 import styles from "./Feed.module.css";
+import Loader from "../Loader/Loader";
 
 function Feed({ posts, setPosts }) {
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     const fetchPosts = async () => {
+      setIsLoading(true);
       const data = await getPosts();
       setPosts(data);
+      setIsLoading(false);
     };
 
     fetchPosts();
@@ -16,15 +20,21 @@ function Feed({ posts, setPosts }) {
 
   return (
     <div className={styles.feed}>
-      {posts.length > 0 ? (
-        <>
-          {posts.map((post) => (
-            <Post key={post.id} post={post} />
-          ))}
-        </>
+      {isLoading ? (
+        <Loader />
       ) : (
         <>
-          <h3> Nenhum post para exibir no momento...</h3>
+          {posts.length > 0 ? (
+            <>
+              {posts.map((post) => (
+                <Post key={post.id} post={post} />
+              ))}
+            </>
+          ) : (
+            <>
+              <h3> Nenhum post para exibir no momento...</h3>
+            </>
+          )}
         </>
       )}
     </div>
